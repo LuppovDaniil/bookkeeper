5b118570e52d87c2246addfba272d432e7cc7470
LuppovDaniil <luppov.dv@phystech.edu>
2023-03-02 22:50:33 +0300

    add and get methods for sqlite are implemented

---
+++ bookkeeper/repository/sqlite_repository.py
@@ -0,0 +1,68 @@
+"""
+Модуль описывает репозиторий, работающий в СУБД sqlite
+"""
+
+from itertools import count
+from typing import Any
+from inspect import get_annotations
+from bookkeeper.utils import adapters
+import sqlite3
+
+from bookkeeper.repository.abstract_repository import AbstractRepository, T
+
+class SqliteRepository(AbstractRepository[T]):
+    """
+    Репозиторий, работающий в sqlite. Хранит данные в базе данных.
+    """
+
+    def __init__(self, db_file: str, cls: type) -> None:
+
+        self.db_file = db_file
+        self.table_name = cls.__name__.lower()
+        self.fields = get_annotations(cls, eval_str=True)
+        self.fields.pop('pk')
+
+    def add(self, obj: T) -> int:
+
+        names = ', '.join(self.fields.keys())
+        p = ', '.join("?" * len(self.fields))
+        values = [getattr(obj, x) for x in self.fields]
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'INSERT INTO {self.table_name} ({names}) VALUES ({p})', values
+            )
+            obj.pk = cur.lastrowid
+        con.close()
+        return obj.pk
+
+    def get(self, pk: int) -> T | None:
+        with sqlite3.connect(self.db_file) as con:
+            con.row_factory = sqlite3.Row
+            cur = con.cursor()
+            cur.execute('PRAGMA foreign_keys = ON')
+            cur.execute(
+                f'SELECT * FROM {self.table_name} WHERE pk=(?)', [pk]
+            )
+            res = cur.fetchall()
+        con.close()
+        adapter = adapters[self.table_name]
+
+        return adapter(res[-1])
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        pass
+
+    def update(self, obj: T) -> None:
+        pass
+
+    def delete(self, pk: int) -> None:
+        pass
+
+
+
+
+
+        
+
--- bookkeeper/utils.py
+++ bookkeeper/utils.py
@@ -3,6 +3,7 @@
 """
 
 from typing import Iterable, Iterator
+from bookkeeper.models.expense import Expense
 
 
 def _get_indent(line: str) -> int:
@@ -61,3 +62,12 @@ def read_tree(lines: Iterable[str]) -> list[tuple[str, str | None]]:
         last_name = name
         last_indent = indent
     return result
+
+adapters = {
+    'expense': lambda x: Expense(pk=x['pk'],
+                                  amount=x['amount'],
+                                  expense_date=x['expense_date'],
+                                  added_date=x['added_date'],
+                                  comment=x['comment'],
+                                  category=x['category'])
+}
